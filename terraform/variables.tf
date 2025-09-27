180cbc1581397aaa4f3dd81b9544ae72ca3d581b
Stephon Treadwell <treadtech33@gmail.com>
2025-09-27 18:21:46 -0400

    added instance type variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,17 @@
 variable "aws_region" {
-  type = string 
-  default = "us-east-1"
+  type        = string
+  default     = "us-east-1"
   description = "The default region where resources will be created"
 }
 
 variable "aws_ami" {
-  type = string
-  default = "ami-0fd3ac4abb734302a"
+  type        = string
+  default     = "ami-0fd3ac4abb734302a"
   description = "The Red Hat Linux free tier eligible ami"
+}
+
+variable "instance_type" {
+  type        = string
+  default     = "t2.micro"
+  description = "The free tier eligible instance type"
 }
